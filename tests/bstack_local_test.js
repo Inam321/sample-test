ae067f55ef3734e50432922794d18c1ef3147247
Inam321 <117627462+Inam321@users.noreply.github.com>
2024-12-05 14:52:06 +0500

    bstack_local_test.js

--- tests/bstack_local_test.js
+++ tests/bstack_local_test.js
@@ -5,6 +5,8 @@ test.describe('test',async()=>{
 test('Non AD User Login & Logout', async ({ page }) => {
     //Application URL
     await page.goto('https://stg.empowergpt.ai/');
+    const hold = ms => new Promise(resolve => setTimeout(resolve, ms))
+    await hold(15000)
     //await page.goto('https://stg.empowergpt.ai/auth/realms/expertadvisor/protocol/openid-connect/auth?client_id=expertadvisor&redirect_uri=https%3A%2F%2Fstaging.empowergpt.ai%2F&state=8eb53f0b-6fcf-4f5e-861e-bc4e376e7b9c&response_mode=fragment&response_type=code&scope=openid&nonce=653d6a0f-e76e-4382-bc47-e910144daeda');
     await CommonMethodsClass.login(page);
     await expect(page.getByText('Data Library')).toBeVisible('Invalid email or password.');
