cdd25ee769e256bf57763eaa53f8342fda820692
Inam321 <117627462+Inam321@users.noreply.github.com>
2024-12-05 14:13:37 +0500

    stack_local_test.js

--- tests/bstack_local_test.js
+++ tests/bstack_local_test.js
@@ -1,7 +1,5 @@
 const { expect, test } = require('@playwright/test');
 const sleep = require('util').promisify(setTimeout)
 test('BStack local is working', async ({ page }) => {
-  await page.goto('http://bs-local.com:45454/');
-  const title = await page.title();
-  expect(title).toContain('BrowserStack Local');
+  await page.goto('https://www.google.com/');
 });
